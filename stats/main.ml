c41b254cb886f8078212bfe2b5c35d96db591ebb
David Scott <dave@recoil.org>
2015-06-18 18:38:18 +0100

    Add very hacky graph rendering code
    Signed-off-by: David Scott <dave@recoil.org>

--- stats/main.ml
+++ stats/main.ml
@@ -15,143 +15,86 @@ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 open Lwt
 
-let d = Dom_html.document
-let get_by_id id =
-  Js.Opt.get (d##getElementById(Js.string id))
-    (fun () -> assert false)
+let do_get ~uri =
+  let method_ = "GET" in
+  let (res, w) = Lwt.task () in
+  let req = XmlHttpRequest.create () in
 
-let multichart name =
-  let _ =
-    C3.Line.make ~kind:`XY ()
-    |> C3.Line.add_group
-       ~segments: [ C3.Segment.make ~kind:`Area_step
-                    ~points:[ 0.1,0.1; 0.2,0.2; 0.3,0.3; 0.4,0.2; 0.5,0.1]
-                    ~label:"Area_step 1" ();
-                    C3.Segment.make ~kind:`Area_step
-                    ~points:[ 0.1,0.1; 0.2,0.2; 0.3,0.3; 0.4,0.2; 0.5,0.1]
-                    ~label:"Area_step 2" (); ]
-    |> C3.Line.add
-       ~segment:(C3.Segment.make ~kind:`Line
-                 ~points:[ 0.1,0.5; 0.2,0.4; 0.3,0.3; 0.4,0.2; 0.5,0.1]
-                 ~label:"Line" ())
-    |> C3.Line.add
-       ~segment:(C3.Segment.make ~kind:`Bar
-                 ~points:[ 0.1,0.1; 0.2,0.1; 0.3,0.1; 0.4,0.1; 0.5,0.1]
-                 ~label:"Bar" ())
-    |> C3.Line.render ~bindto:name in
-  ()
+  Firebug.console##log(Js.string (Uri.to_string uri));
+  req##_open (Js.string method_, Js.string (Uri.to_string uri), Js._true);
+  req##onreadystatechange <- Js.wrap_callback
+    (fun _ ->
+       (match req##readyState with
+                   | XmlHttpRequest.DONE ->
+                           Lwt.wakeup w (Js.to_string req##responseText)
+                   | _ -> ()));
 
+	req##send (Js.some (Js.string ""));
+  Lwt.on_cancel res (fun () -> req##abort ()) ;
+  res
 
-let xychart kind name =
-  let _ =
-    C3.Line.make ~kind:`XY ()
-    |> C3.Line.add
-       ~segment:(C3.Segment.make ~kind ~points:[0.1,0.1; 0.2,0.2; 0.3,0.3; 0.4,0.2; 0.5,0.1]
-                 ~label:(C3.Segment.string_of_kind kind) ())
-    |> C3.Line.render ~bindto:name in
-  ()
+let charts = Hashtbl.create 7
 
-let piechart name donut =
-  let _ =
-    C3.Pie.make
-      ?hole:(if donut then Some "hello" else None)
-      ~sectors:[ "a", 30.; "b", 120.; "c", 15.; "d", 90. ] ()
-    |> C3.Pie.render ~bindto:name in
-  ()
+let render_update update =
+  (* XXX: pick a memory free RRD for now *)
+  let open Rrd_updates in
+	let _, legends = Array.fold_left
+	  (fun (idx, acc) elt ->
+      match Xen_api_metrics.Legend.of_string elt with
+       | `Ok (name, _, _, _) ->
+         (idx + 1, (idx, name) :: acc)
+       | _ ->
+         (idx + 1, acc)
+    ) (0, []) update.legend in
+  let data = Array.to_list update.data in
+  List.iter
+    (fun (idx, legend) ->
+  	  (* Firebug.console##log(Js.string(Printf.sprintf "legends = %d; nrows = %d; nrow_datas = [ %s ]" (Array.length update.legend) (Array.length update.data) (String.concat "; " (List.map (fun x -> string_of_int (Array.length x.row_data)) (Array.to_list update.data)))));
+    *)
+	  	let points = List.map (fun x -> x.time, x.row_data.(idx)) data in
+      (* Filter out Nans *)
+	  	let points = List.filter (fun (_, x) -> classify_float x <> FP_nan) points in
+	  	Firebug.console##log(Js.string (Printf.sprintf "points = [| %s |]" (String.concat "; " (List.map (fun (t, p) -> Printf.sprintf "%Ld %f" t p) points))));
+      (* XXX: I have no idea what these values mean! *)
+	  	(*let point = sin(time /. 60.) *. point *. 0.2 +. point *. 0.8 in*)
+      let chart =
+        if Hashtbl.mem charts legend
+        then Hashtbl.find charts legend
+        else begin
+          let chart =
+            C3.Line.make ~kind:`Timeseries ~x_format:"%m/%d" ()
+            |> C3.Line.render ~bindto:("#" ^ legend) in
+          Hashtbl.add charts legend chart;
+          chart
+        end in
+      C3.Line.flow ~segments:[ C3.Segment.make ~label:legend ~points:(List.map (fun (t, v) -> Int64.to_float t, v) points)
+                               ~kind:`Area_step () ]
+                   ~flow_to:(`Delete 1)
+                   chart;
+    ) legends
 
-let gauge name =
-  let _ =
-    C3.Gauge.make
-      ~thresholds:[ 30., "#FF0000"; 60., "#F97600"; 90., "#F6C600"; 100., "#60B044" ]
-      ~label:"hello"
-      ~value:60.
-      ()
-    |> C3.Gauge.render ~bindto:name in
-  ()
+let watch_rrds () =
+  let uri start =
+    let query = [ "start", [ string_of_int start ] ] in
+    Uri.make ~scheme:"http" ~path:"/rrd_updates" ~query () in
 
-
-
-module Histogram = struct
-  type t = {
-    bins: int array;
-    start: float;
-    width: float;
-  }
-  let make ~start ~width ~n () =
-    let bins = Array.make n 0 in
-    { start; width; bins }
-  let add ~value t =
-    try
-      (* Discard values which are out of range *)
-      let bin = int_of_float ((value -. t.start) /. t.width) in
-      t.bins.(bin) <- t.bins.(bin) + 1
-    with _ -> ()
-  let to_segment t =
-    let points =
-      t.bins
-      |> Array.to_list
-      |> List.mapi (fun idx v -> let t = float_of_int idx *. t.width +. t.start in t,float_of_int v) in
-
-    C3.Segment.make ~label:"histogram" ~points ~kind:`Bar ()
-end
-
-let pi = 4.0 *. atan(1.0)
-
-let normal () =
-  let h = Histogram.make ~start:(-1.) ~width:0.01 ~n:200 () in
-  let chart =
-    C3.Line.make ~kind:`XY ()
-    |> C3.Line.add ~segment:(Histogram.to_segment h)
-    |> C3.Line.render ~bindto:"#normal" in
-  let sd = 0.1 in
-  Lwt.async
-    (fun () ->
-      let rec loop iterations =
-        for i = 0 to 250 do
-          let u_1 = Random.float 1. in
-          let u_2 = Random.float 1. in
-          (* Box-Muller transform *)
-          let z_0 = sqrt (-2.0 *. log(u_1)) *. cos(2. *. pi *. u_2) *. sd in
-          let z_1 = sqrt (-2.0 *. log(u_1)) *. sin(2. *. pi *. u_2) *. sd in
-          Histogram.add ~value:z_0 h;
-          Histogram.add ~value:z_1 h;
-        done;
-        C3.Line.update ~segments:[ Histogram.to_segment h ] chart;
-        Lwt_js.sleep 1.
-        >>= fun () ->
-        loop (iterations + 1) in
-      loop 0
-    )
-
-let timeseries () =
-  let chart =
-    C3.Line.make ~kind:`Timeseries ~x_format:"%m/%d" ()
-    |> C3.Line.render ~bindto:"#timeserieschart" in
-  let rec update_graph_forever chart t () =
-    if t > 10. then return ()
-    else begin
-      C3.Line.flow ~segments:[ C3.Segment.make ~label:"sin(t)" ~points:[t, sin t]
-                                 ~kind:`Area_step () ]
-                     ~flow_to:(`Delete 0)
-                     chart;
-      Lwt_js.sleep 0.1
-      >>= fun () ->
-      update_graph_forever chart (t +. 0.1) ()
-    end in
-  Lwt.async (update_graph_forever chart 0.)
+  let rec loop start =
+    do_get ~uri:(uri start)
+    >>= fun txt ->
+    let update = Xen_api_metrics.Updates.parse txt in
+    Firebug.console##log(Js.string "got some updates");
+    render_update update;
+    Lwt_js.sleep 5.
+    >>= fun () ->
+    loop (Int64.to_int update.Rrd_updates.end_time) in
+  (* XXX: query server's current clock *)
+  do_get ~uri:(uri 1500000000) >>= fun txt ->
+  let update = Xen_api_metrics.Updates.parse txt in
+  loop ((Int64.to_int update.Rrd_updates.end_time) - 60*9)
 
 let _ =
   Dom_html.window##onload <- Dom_html.handler
     (fun _ ->
-      multichart "#multichart";
-      piechart "#piechart" false;
-      piechart "#donutchart" true;
-      gauge "#gauge";
-      xychart `Line "#xychart";
-      xychart `Area "#xyareachart";
-      xychart `Area_step "#xyareastepchart";
-      xychart `Spline "#xysplinechart";
-      normal ();
-      timeseries ();
+      Lwt.async watch_rrds;
       Js._true
     )
